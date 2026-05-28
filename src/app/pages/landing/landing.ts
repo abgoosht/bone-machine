c963be6919b38271d64af6293c46fcc929135771
“Mohsen” <seyed.naini@gmail.com>
2026-05-28 18:24:22 -0500

    feat: player name fields with defaults and live editing
    Closes #19
    
    -Claudia

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, effect } from '@angular/core';
 import { Slider } from '../../components/slider/slider';
+import { defaultPlayerNames } from './landing.constants';
 
 @Component({
   selector: 'bm-landing',
@@ -11,6 +12,22 @@ export class Landing {
   readonly scoreOptions = [150, 200, 250, 300, 500];
   readonly playerCountOptions = [2, 3, 4];
 
-  targetScore = signal(300);
+  targetScore = signal(150);
   playerCount = signal(2);
+  playerNames = signal([...defaultPlayerNames[2]]);
+
+  constructor() {
+    effect(() => {
+      this.playerNames.set([...defaultPlayerNames[this.playerCount()]]);
+    });
+  }
+
+  updatePlayerName(index: number, event: Event): void {
+    const name = (event.target as HTMLInputElement).value;
+    this.playerNames.update(names => {
+      const updatedNames = [...names];
+      updatedNames[index] = name;
+      return updatedNames;
+    });
+  }
 }
